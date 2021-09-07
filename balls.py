edfe003e1bf7de4cf7bb2af64dd2584b80efd444
Lipavskiy Alexey <lipavskiy777@gmail.com>
2021-09-08 00:04:32 +0300

    test commit, canvas size changed

--- balls.py
+++ balls.py
@@ -9,25 +9,27 @@ Created on Mon Sep  6 22:32:50 2021
 import tkinter as tk
 import random
 
-HEIGHT = 200
-WIDTH = 300
+HEIGHT = 400
+WIDTH = 500
+
 
 class Ball:
     def __init__(self):
         self.R = random.randint(3, 11)
         self.x = random.randint(self.R, WIDTH - self.R)
         self.y = random.randint(self.R, HEIGHT - self.R)
-        
+
         self.dx = random.randint(1, 15)
         self.dy = random.randint(1, 16)
         self.color = random.choice(['blue', 'green', 'red', 'brown'])
         self.id = canvas.create_oval(
-                                self.x - self.R,
-                                self.y - self.R,
-                                self.x + self.R,
-                                self.y + self.R,
-                                fill=self.color
-                                )
+            self.x - self.R,
+            self.y - self.R,
+            self.x + self.R,
+            self.y + self.R,
+            fill=self.color
+        )
+
     def move(self):
         self.x += self.dx
         self.y += self.dy
@@ -35,6 +37,7 @@ class Ball:
             self.dx = -self.dx
         if self.y - self.R <= 0 or self.y + self.R >= HEIGHT:
             self.dy = -self.dy
+
     def show(self):
         canvas.move(self.id, self.dx, self.dy)
 
@@ -48,7 +51,7 @@ def tick():
 
 def main():
     global root, canvas, balls
-    
+
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
     canvas = tk.Canvas(root)
@@ -57,9 +60,7 @@ def main():
     balls = [Ball() for i in range(45)]
     tick()
     root.mainloop()
-    
 
 
 if __name__ == "__main__":
     main()
-
